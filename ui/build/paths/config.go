9bd38c1975fbc5579f011574f9a2f23cdfc7487d
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-05-02 03:32:29 +0530

    soong: Whitelist date
    Change-Id: I773b3bbf01d4b44ed67714a5c3ce90c0ac8d6463
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -108,7 +108,7 @@ var Configuration = map[string]PathConfig{
 	"tr":          Allowed,
 	"unzip":       Allowed,
 	"zip":         Allowed,
-
+	"date":		   Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
