4b4127153b86f6c30ba94cedb17afd421b7d50ea
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-05-10 23:07:33 +0530

    soong: Allowed tar
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -109,6 +109,7 @@ var Configuration = map[string]PathConfig{
 	"unzip":       Allowed,
 	"zip":         Allowed,
 	"date":		   Allowed,
+	"tar":		   Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
